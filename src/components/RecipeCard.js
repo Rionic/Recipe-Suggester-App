8da1dc1a0c336dc94e94466c6f3d6b23b1422262
rsg23 <rajangrewal123@gmail.com>
2023-12-13 19:19:07 -0800

    add accordion and improve card styling

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css'
-import { Card, CardMedia, Typography } from '@mui/material';
+import { Card, CardMedia, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function RecipeCard(props) {
+    const [expanded, setExpanded] = useState(false);
+    const handleExpansion = () => {
+        setExpanded(!expanded);
+    };
+
     console.log(props.ingredients);
     return (
-        <Card className="card-media">
+        <Card className={`card-media ${expanded ? 'expanded-card' : ''}`}>
             <CardMedia
                 component="img"
                 height="200"
@@ -13,13 +19,40 @@ function RecipeCard(props) {
                 alt={props.title}
             />
             <Typography variant="h5">{props.title}</Typography>
-            <Typography variant="body1">{props.description}</Typography>
-            {props.ingredients && props.ingredients.map((ingredient, index) => (
-                <div>
-                    <Typography variant="body1">{ingredient.amount}</Typography>
-                    <Typography variant="body1">{ingredient.name}</Typography>
-                </div>
-            ))}
+            <Typography style={{
+                    margin: '20px',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    display: '-webkit-box',
+                    WebkitLineClamp: 2,
+                    WebkitBoxOrient: 'vertical',
+                }}
+                variant="body1"
+                align={'left'}
+            >
+                {props.description}
+            </Typography>
+            <Accordion
+                className="ingredients-accordion"
+                expanded={expanded} onChange={handleExpansion}
+                style={{width: '100%', position: 'relative', bottom: '0px'}}
+                sx={{boxShadow: 0}}
+            >
+                <AccordionSummary 
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Ingredients</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {props.ingredients && props.ingredients.map((ingredient, index) => (
+                        <div key={index}>
+                            <Typography variant="body1">{ingredient.amount + ' '}{ingredient.name}</Typography>
+                        </div>
+                    ))}
+                </AccordionDetails>
+            </Accordion>
         </Card>
     ); // Next task: grab measurements, and make display nice
 }
